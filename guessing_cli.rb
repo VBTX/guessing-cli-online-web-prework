d2bcfeb0a25bcbeae20bf7a9bc290f76debdebaa
Victoria B <viktorija.bourassa@gmail.com>
2018-12-27 21:11:43 +0000

    Automatically backed up by Learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -1,10 +1,10 @@
 def run_guessing_game
-
+puts "Guess a number between 1 and 6."
   input = gets.chomp
   number = rand(1..6)
   case input
   when " "
-    puts "Guess a number between 1 and 6."
+
   when number
       puts "You guessed the correct number!"
   when input != number

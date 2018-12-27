9555ffcbf74b3eee1012a94d9d51b20df4a0eeb8
Victoria B <viktorija.bourassa@gmail.com>
2018-12-27 21:09:49 +0000

    Automatically backed up by Learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -1,9 +1,9 @@
 def run_guessing_game
-  
+
   input = gets.chomp
   number = rand(1..6)
     if input == number
-      puts "You guessed the correct number!"
+      return "You guessed the correct number!"
     elsif input == 'exit'
       puts "Goodbye!"
     elsif input == number
